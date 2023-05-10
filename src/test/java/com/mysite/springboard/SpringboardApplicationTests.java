bf828f060c770ae48f4ecf0fdfa064a1de00eef7
younjin0520 <younjin0520@naver.com>
2023-05-10 20:18:50 +0900

    [sbb] 테스트 코드 추가

--- src/test/java/com/mysite/springboard/SpringboardApplicationTests.java
+++ src/test/java/com/mysite/springboard/SpringboardApplicationTests.java
@@ -1,13 +1,29 @@
 package com.mysite.springboard;
 
+import com.mysite.springboard.Repository.QuestionRepository;
+import com.mysite.springboard.entity.Question;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SpringboardApplicationTests {
 
+	@Autowired
+	private QuestionRepository questionRepository;
+
 	@Test
-	void contextLoads() {
+	void testJpa() {
+		List<Question> all = this.questionRepository.findAll();
+		assertEquals(6, all.size());
+
+		Question q = all.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
 	}
 
 }
