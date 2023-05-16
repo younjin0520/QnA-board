13b2cd99ebe045318ee69dbf2516f6c87fd0af7d
younjin0520 <younjin0520@naver.com>
2023-05-16 20:41:28 +0900

    [sbb] URL prefix (2-10)

--- src/main/java/com/mysite/springboard/question/QuestionController.java
+++ src/main/java/com/mysite/springboard/question/QuestionController.java
@@ -5,16 +5,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
 
     private final QuestionService questionService;
 
-    @GetMapping("question/list")
+    @GetMapping("/list")
     public String list(Model model) {
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);   // questionList라는 이름으로 값 저장
@@ -22,7 +24,7 @@ public class QuestionController {
     }
 
     //상세페이지 매핑
-    @GetMapping(value = "/question/detail/{id}")
+    @GetMapping(value = "/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
