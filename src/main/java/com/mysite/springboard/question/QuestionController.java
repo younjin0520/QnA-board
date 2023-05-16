5833f590e2687edb511d0741de378ea51453297b
younjin0520 <younjin0520@naver.com>
2023-05-16 20:19:29 +0900

    [sbb] 상세화면 연결 (2-10)

--- src/main/java/com/mysite/springboard/question/QuestionController.java
+++ src/main/java/com/mysite/springboard/question/QuestionController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -18,5 +19,11 @@ public class QuestionController {
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);   // questionList라는 이름으로 값 저장
         return "question_list"; // question_list.html 반환
+    }
+
+    //상세페이지 매핑
+    @GetMapping(value = "/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        return "question_detail";
     }
 }
