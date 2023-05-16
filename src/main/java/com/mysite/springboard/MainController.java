ab492e387432ac1cec7124d658db06124d465a77
younjin0520 <younjin0520@naver.com>
2023-05-16 19:51:46 +0900

    [sbb] ROOT URL 수정 (2-08)

--- src/main/java/com/mysite/springboard/MainController.java
+++ src/main/java/com/mysite/springboard/MainController.java
@@ -11,4 +11,9 @@ public class MainController {
     public String index() {
         return "안녕하세요, SBB에 오신것을 환영합니다.";
     }
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/question/list";
+    }
 }
