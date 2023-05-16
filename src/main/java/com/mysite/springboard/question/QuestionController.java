0ddf9767a99c60e78a91b739e62a4bda24e62081
younjin0520 <younjin0520@naver.com>
2023-05-16 20:04:44 +0900

    [sbb] service 사용 (2-09)

--- src/main/java/com/mysite/springboard/question/QuestionController.java
+++ src/main/java/com/mysite/springboard/question/QuestionController.java
@@ -11,11 +11,11 @@ import java.util.List;
 @Controller
 public class QuestionController {
 
-    private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
 
     @GetMapping("question/list")
     public String list(Model model) {
-        List<Question> questionList = this.questionRepository.findAll();
+        List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);   // questionList라는 이름으로 값 저장
         return "question_list"; // question_list.html 반환
     }
