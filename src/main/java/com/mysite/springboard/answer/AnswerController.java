d002d84486c1c14761f6bb4def8c811baa5ad4a8
younjin0520 <younjin0520@naver.com>
2023-05-19 19:28:05 +0900

    [sbb] Answer 컨트롤러 추가 (2-11)

---
+++ src/main/java/com/mysite/springboard/answer/AnswerController.java
@@ -0,0 +1,25 @@
+package com.mysite.springboard.answer;
+
+import com.mysite.springboard.question.Question;
+import com.mysite.springboard.question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("/answer")
+@RequiredArgsConstructor
+@Controller
+public class AnswerController {
+    private final QuestionService questionService;
+
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam String content) {
+        Question question = this.questionService.getQuestion(id);
+        // 답변을 저장한다.
+        return String.format("redirect:/question/detail/%s", id);
+    }
+}
