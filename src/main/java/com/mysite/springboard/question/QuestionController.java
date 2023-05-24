4052b468c7f212e2dd8fe7b9426a7fd4360985c9
younjin0520 <younjin0520@naver.com>
2023-05-24 20:42:19 +0900

    [sbb] 질문 생성 페이지 개발 (2-15)

--- src/main/java/com/mysite/springboard/question/QuestionController.java
+++ src/main/java/com/mysite/springboard/question/QuestionController.java
@@ -30,4 +30,9 @@ public class QuestionController {
         model.addAttribute("question", question);
         return "question_detail";
     }
+
+    @GetMapping("/create")
+    public String questionCreate() {
+        return "question_form";
+    }
 }
