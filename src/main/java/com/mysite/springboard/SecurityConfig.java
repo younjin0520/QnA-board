9fa28026d18eea37136b62b71833bc0256a42085
younjin0520 <younjin0520@naver.com>
2023-06-10 05:26:11 +0900

    [sbb] 스프링 시큐리티 - H2 예외처리 (3-4)

--- src/main/java/com/mysite/springboard/SecurityConfig.java
+++ src/main/java/com/mysite/springboard/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -14,7 +15,8 @@ public class SecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests().requestMatchers(
                 new AntPathRequestMatcher("/**")).permitAll()
-                .and().csrf().ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+                .and().csrf().ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**"))
+                .and().headers().addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN));
         return http.build();
     }
 }
